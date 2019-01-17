52124d1f4ddfae2a9a01a80d8d1daad121d2af22
Brent  Ransom <sadumedusa@gmail.com>
2019-01-17 01:54:54 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,7 +1,9 @@
 class CreateStudents < ActiveRecord::Migration[5.1]
-  create_table :students do |t|
+  
     
   def Change
-    t.string :name
+    create_table :students do |t|
+      t.string :name
+    end
   end
 end
